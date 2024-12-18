0600c443a841347b7afc2c03da61a45dfead4384
Pietro Novelli <pietronvll@gmail.com>
2024-12-18 20:12:52 +0100

    Added predict

--- src/operator_learning/kernel/base.py
+++ src/operator_learning/kernel/base.py
@@ -1,6 +1,8 @@
 import logging
+from math import sqrt
 from typing import TypedDict
 
+import numpy as np
 from numpy.typing import ArrayLike
 
 logger = logging.getLogger("operator_learning")
@@ -12,16 +14,20 @@ class FitResult(TypedDict):
     svals: ArrayLike | None
 
 
-def postprocess_UV(
-    U: ArrayLike, V: ArrayLike, rank: int
-) -> tuple[ArrayLike, ArrayLike]:
-    assert U.shape == V.shape
-    if U.shape[1] < rank:
-        logger.warning(
-            f"Warning: The fitting algorithm discarded {rank - U.shape[1]} dimensions of the {rank} requested out of numerical instabilities.\nThe rank attribute has been updated to {U.shape[1]}.\nConsider decreasing the rank parameter."
-        )
-    else:
-        # Assuming that everything is in decreasing order
-        U = U[:, :rank]
-        V = V[:, :rank]
-    return U, V
+def predict(
+    num_steps: int,  # Number of steps to predict (return the last one)
+    fit_result: FitResult,
+    K_YX: np.ndarray,  # Kernel matrix between the output data and the input data
+    K_Xin_X: np.ndarray,  # Kernel matrix between the initial conditions and the input data
+    obs_train_Y: np.ndarray,  # Observable to be predicted evaluated on the output training data
+) -> np.ndarray:
+    # G = S UV.T Z
+    # G^n = (SU)(V.T K_YX U)^(n-1)(V.T Z)
+    U = fit_result["U"]
+    V = fit_result["V"]
+    npts = U.shape[0]
+    K_dot_U = K_Xin_X @ U / sqrt(npts)
+    V_dot_obs = V.T @ obs_train_Y / sqrt(npts)
+    V_K_YX_U = np.linalg.multi_dot([V.T, K_YX, U]) / npts
+    M = np.linalg.matrix_power(V_K_YX_U, num_steps - 1)
+    return np.linalg.multi_dot([K_dot_U, M, V_dot_obs])

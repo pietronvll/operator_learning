1aa115f0edd41b5f7b676d8ef36454dfdc84b2ea
Pietro Novelli <pietronvll@gmail.com>
2024-12-18 20:22:00 +0100

    Added eig

--- src/operator_learning/linalg.py
+++ src/operator_learning/linalg.py
@@ -46,3 +46,27 @@ def rank_reveal(
                 f"Warning: Discarted {rank - values.shape[0]} dimensions of the {rank} requested due to numerical instability. Consider decreasing the rank. The largest discarded value is: {_first_discarded_val:.3e}."
             )
         return top_idxs[valid]
+
+def weighted_norm(A: ArrayLike, M: ArrayLike | None = None):
+    r"""Weighted norm of the columns of A.
+
+    Args:
+        A (ndarray): 1D or 2D array. If 2D, the columns are treated as vectors.
+        M (ndarray or LinearOperator, optional): Weigthing matrix. the norm of the vector :math:`a` is given by
+        :math:`\langle a, Ma\rangle`. Defaults to None, corresponding to the Identity matrix. Warning: no checks are
+        performed on M being a PSD operator.
+
+    Returns:
+        (ndarray or float): If ``A.ndim == 2`` returns 1D array of floats corresponding to the norms of
+        the columns of A. Else return a float.
+    """
+    assert A.ndim <= 2, "'A' must be a vector or a 2D array"
+    if M is None:
+        norm = np.linalg.norm(A, axis=0)
+    else:
+        _A = np.dot(M, A)
+        _A_T = np.dot(M.T, A)
+        norm = np.real(np.sum(0.5 * (np.conj(A) * _A + np.conj(A) * _A_T), axis=0))
+    rcond = 10.0 * A.shape[0] * np.finfo(A.dtype).eps
+    norm = np.where(norm < rcond, 0.0, norm)
+    return np.sqrt(norm)
--- src/operator_learning/utils.py
+++ src/operator_learning/utils.py
@@ -1,5 +1,8 @@
+from math import sqrt
+
 import numpy as np
 from numpy.typing import ArrayLike
+from scipy.spatial.distance import pdist
 
 
 def topk(vec: ArrayLike, k: int):
@@ -9,3 +12,31 @@ def topk(vec: ArrayLike, k: int):
     indices = sort_perm[:k]
     values = vec[indices]
     return values, indices
+
+
+def fuzzy_parse_complex(vec: ArrayLike, tol: float = 10.0):
+    assert issubclass(
+        vec.dtype.type, np.complexfloating
+    ), "The input element should be complex"
+    rcond = tol * np.finfo(vec.dtype).eps
+    pdist_real_part = pdist(vec.real[:, None])
+    # Set the same element whenever pdist is smaller than eps*tol
+    condensed_idxs = np.argwhere(pdist_real_part < rcond)[:, 0]
+    fuzzy_real = vec.real.copy()
+    if condensed_idxs.shape[0] >= 1:
+        for idx in condensed_idxs:
+            i, j = row_col_from_condensed_index(vec.real.shape[0], idx)
+            avg = 0.5 * (fuzzy_real[i] + fuzzy_real[j])
+            fuzzy_real[i] = avg
+            fuzzy_real[j] = avg
+    fuzzy_imag = vec.imag.copy()
+    fuzzy_imag[np.abs(fuzzy_imag) < rcond] = 0.0
+    return fuzzy_real + 1j * fuzzy_imag
+
+
+def row_col_from_condensed_index(d, index):
+    # Credits to: https://stackoverflow.com/a/14839010
+    b = 1 - (2 * d)
+    i = (-b - sqrt(b**2 - 8 * index)) // 2
+    j = index + i * (b + i + 2) // 2 + 1
+    return (int(i), int(j))

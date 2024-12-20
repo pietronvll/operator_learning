3ecea30cc0ca85c45cf4d03ae069a792702d0d99
Pietro Novelli <pietronvll@gmail.com>
2024-12-20 17:58:30 +0100

    Added vamp, dpnets, spectral_contrastive scores

--- src/operator_learning/nn/functional.py
+++ src/operator_learning/nn/functional.py
@@ -1,6 +1,9 @@
+from typing import Literal
+
 import torch
 
 from operator_learning.nn.linalg import covariance, sqrtmh
+from operator_learning.structs import EigResult, FitResult
 
 
 def vamp_score(X, Y, schatten_norm: int = 2, center_covariances: bool = True):
@@ -113,3 +116,69 @@ def logfro_loss(cov: torch.Tensor):
     vals_x = torch.where(vals_x > eps, vals_x, eps)
     loss = torch.mean(-torch.log(vals_x) + vals_x * (vals_x - 1.0))
     return loss
+
+
+def fit_ridgels(
+    cov_X: torch.Tensor,
+    tikhonov_reg: float = 0.0,
+) -> FitResult:
+    """Fit the ridge least squares estimator for the transfer operator.
+
+    Args:
+        cov_X (torch.Tensor): covariance matrix of the input data.
+        tikhonov_reg (float, optional): Ridge regularization. Defaults to 0.0.
+
+    Returns:
+        FitResult: as defined in operator_learning.structs
+    """
+    dim = cov_X.shape[0]
+    reg_input_covariance = cov_X + tikhonov_reg * torch.eye(
+        dim, dtype=cov_X.dtype, device=cov_X.device
+    )
+    values, vectors = torch.linalg.eigh(reg_input_covariance)
+    # Divide columns of vectors by square root of eigenvalues
+    rsqrt_evals = 1.0 / torch.sqrt(values + 1e-10)
+    Q = vectors @ torch.diag(rsqrt_evals)
+    result: FitResult = FitResult({"U": Q, "V": Q, "svals": values})
+    return result
+
+
+def eig(
+    fit_result: FitResult,
+    cov_XY: torch.Tensor,
+) -> EigResult:
+    """Computes the eigendecomposition of the transfer operator. It only computes the right eigenvectors.
+
+    Args:
+        fit_result (FitResult): Fit result from the fit_ridgels function.
+        cov_XY (torch.Tensor): Cross covariance matrix between the input and output data.
+
+    Returns:
+        EigResult: as defined in operator_learning.structs
+    """
+    U = fit_result["U"]
+    # Using the trick described in https://arxiv.org/abs/1905.11490
+    M = torch.linalg.multi_dot([U.T, cov_XY, U])
+    values, rv = torch.linalg.eig(M)
+
+    # Normalization in RKHS norm
+    rv = U.cfloat() @ rv
+    rv = rv / torch.linalg.norm(rv, axis=0)
+    # # Biorthogonalization
+    # lv = torch.linalg.multi_dot([cov_XY.T, U, lv])
+    # lv = lv[:, l_perm]
+    # l_norm = torch.sum(lv * rv, axis=0)
+    # lv = lv / l_norm
+    result: EigResult = EigResult({"values": values, "left": None, "right": rv})
+    return result
+
+
+def evaluate_eigenfunction(
+    X: torch.Tensor,
+    eig_result: EigResult,
+    which: Literal["left", "right"] = "right",
+):
+    if which == "left":
+        raise NotImplementedError("Left eigenfunctions are not implemented")
+    vr_or_vl = eig_result[which]
+    return X.to(vr_or_vl.dtype) @ vr_or_vl
--- src/operator_learning/structs.py
+++ src/operator_learning/structs.py
@@ -1,15 +1,16 @@
-from typing import TypedDict
+from typing import TypedDict, Union
 
 from numpy.typing import ArrayLike
+from torch import Tensor
 
 
 class FitResult(TypedDict):
-    U: ArrayLike
-    V: ArrayLike
-    svals: ArrayLike | None
+    U: Union[ArrayLike, Tensor]
+    V: Union[ArrayLike, Tensor]
+    svals: Union[ArrayLike, Tensor] | None
 
 
 class EigResult(TypedDict):
-    values: ArrayLike
-    left: ArrayLike
-    right: ArrayLike
+    values: Union[ArrayLike, Tensor]
+    left: Union[ArrayLike, Tensor] | None
+    right: Union[ArrayLike, Tensor]
